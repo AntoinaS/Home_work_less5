8d5fbbe4671d67fcbe129c9b090c47f89fac1f7c
ASelyova <aselyova@yandex.ru>
2020-10-12 23:48:12 +0300

    поправлен подсчет суммы корзины

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,7 +8,8 @@ class ProductPageLocators:
     product_page_add_to_basket = ".btn-add-to-basket"
     product_page_success_mess = ".alert:nth-child(1) .alertinner strong"
     product_page_name_of_product = ".product_main h1"
-    product_page_basket_value = ".alert:nth-child(3) .alertinner strong"
+    product_page_total_basket_value = ".alert:nth-child(3) .alertinner strong"
+    product_page_basket_value = ".basket-mini"
     product_page_price_of_product = ".product_main .price_color"
     product_page_basket_link = ".alertinner [href$='/basket/']"
 
@@ -16,15 +17,19 @@ class ProductPageLocators:
 class ProductPage(BasePage):
     def add_product_to_basket(self):
         #Act
+        present_value_not_float = self.find(ProductPageLocators.product_page_basket_value).text
+        present_value_float = float((present_value_not_float.split('£')[1]).split('V')[0])
         self.find(ProductPageLocators.product_page_add_to_basket).click()
         #Assert
         success_mess=self.find(ProductPageLocators.product_page_success_mess).text 
         product_name = self.find(ProductPageLocators.product_page_name_of_product).text
         assert product_name == success_mess, "The product name is missing from the message" 
         #Assert
-        basket_value = self.find(ProductPageLocators.product_page_basket_value).text
-        price_product = self.find(ProductPageLocators.product_page_price_of_product).text
-        assert price_product in basket_value, "The product price is not equal to the basket price "
+        basket_value = self.find(ProductPageLocators.product_page_total_basket_value).text
+        price_product_not_float = self.find(ProductPageLocators.product_page_price_of_product).text
+        price_product_float = float(price_product_not_float[1:len(price_product_not_float)])
+        assert str(price_product_float + present_value_float) in basket_value, \
+            f"The product {price_product_float} price is not equal to {present_value_float} the basket price "
     
     def go_to_basket_from_product_page(self):
         #Act
